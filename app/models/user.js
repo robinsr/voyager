6915a0ae7ab38bbe37260011e28fe02199a4c80f
Ryan Robinson <ryan.b.robinson@gmail.com>
2014-07-07 16:57:39 -0700

    updated user model (beautify - fixed this time)

--- app/models/user.js
+++ app/models/user.js
@@ -12,40 +12,39 @@ var mongoose = require('mongoose'),
      */
 
 var UserSchema = new Schema({
-name: {
-    type: String,
-    default: ''
-},
-email: {
-    type: String,
-    default: ''
-},
-username: {
-    type: String,
-    default: ''
-},
-provider: {
-    type: String,
-    default: ''
-},
-hashed_password: {
-    type: String,
-    default: ''
-},
-salt: {
-    type: String,
-    default: ''
-},
-authToken: {
-    type: String,
-    default: ''
-},
-facebook: {},
-twitter: {},
-github: {},
-google: {},
-linkedin: {}
-}]
+    name: {
+        type: String,
+        default: ''
+    },
+    email: {
+        type: String,
+        default: ''
+    },
+    username: {
+        type: String,
+        default: ''
+    },
+    provider: {
+        type: String,
+        default: ''
+    },
+    hashed_password: {
+        type: String,
+        default: ''
+    },
+    salt: {
+        type: String,
+        default: ''
+    },
+    authToken: {
+        type: String,
+        default: ''
+    },
+    facebook: {},
+    twitter: {},
+    github: {},
+    google: {},
+    linkedin: {}
 })
 
 /**
